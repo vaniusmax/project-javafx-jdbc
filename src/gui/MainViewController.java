9b30c013b0894bf96566cfc8fd28251d039aa9f3
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-06-26 20:29:08 -0300

    Desenho da tela departmentList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -32,7 +32,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onmenuItemDepartmentAction() {
-		System.out.println("menuItemDepartment");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
